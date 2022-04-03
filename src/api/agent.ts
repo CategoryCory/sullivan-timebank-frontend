38fb622bead2f12c5e52a622e6407c8f605ab5e1
Cory Coward <cory@c2softwaresolutions.com>
2022-04-03 13:34:18 -0500

    Change name of environment variable

--- src/api/agent.ts
+++ src/api/agent.ts
@@ -13,7 +13,7 @@ import { Skill } from "../models/skill";
 //     })
 // };
 
-axios.defaults.baseURL = process.env.API_URL || "https://localhost:5001/api";
+axios.defaults.baseURL = process.env.REACT_APP_API_KEY || "https://localhost:5001/api";
 
 axios.interceptors.request.use(config => {
     const token = store.commonStore.token;
